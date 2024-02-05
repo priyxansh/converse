68d746c9f05c6924b852adde50a2443e1e0bffca
Priyansh Rai <priyanshrai16@gmail.com>
2024-02-05 23:08:43 +0530

    Add FAQ and Feedback pages

---
+++ src/app/(overview)/faq/page.tsx
@@ -0,0 +1,11 @@
+type FAQPageProps = {};
+
+const FAQPage = ({}: FAQPageProps) => {
+  return (
+    <div>
+      <h1>FAQ</h1>
+    </div>
+  );
+};
+
+export default FAQPage;
---
+++ src/app/(overview)/feedback/page.tsx
@@ -0,0 +1,11 @@
+type FeedbackPageProps = {};
+
+const FeedbackPage = ({}: FeedbackPageProps) => {
+  return (
+    <div>
+      <h1>Feedback</h1>
+    </div>
+  );
+};
+
+export default FeedbackPage;
