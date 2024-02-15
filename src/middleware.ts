354614cf51ed0fbd71c4274d568193e86e49ed6c
Priyansh Rai <priyanshrai16@gmail.com>
2024-02-15 22:33:09 +0530

    Add middleware for authentication and redirection

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  // Get the session from the request
+  const session = req.auth;
+
+  // Get origin and pathname from the request URL
+  const origin = req.nextUrl.origin;
+  const pathname = req.nextUrl.pathname;
+
+  // Redirect to /auth if no session found
+  if (!session && pathname !== "/") {
+    return NextResponse.redirect(origin + "/auth");
+  }
+
+  // Redirect to /auth/set-password if isUserCreated is false
+  if (session && session.user.isUserCreated === false) {
+    return NextResponse.redirect(origin + "/auth/set-password");
+  }
+
+  // Redirect to /complete-profile if isProfileComplete is false
+  if (session && session.user.isProfileComplete === false) {
+    return NextResponse.redirect(origin + "/auth/complete-profile");
+  }
+});
+
+export const config = {
+  matcher: ["/", "/chat/:path*"],
+};
