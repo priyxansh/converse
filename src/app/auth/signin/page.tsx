8a1a7d34207bce64cc0d4f0650f607af8c197083
Priyansh Rai <priyanshrai16@gmail.com>
2024-02-15 15:58:07 +0530

    Add sign-in functionality and components

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,11 +1,40 @@
+import GitHubOAuthButton from "@/components/auth/GitHubOAuthButton";
+import GoogleOAuthButton from "@/components/auth/GoogleOAuthButton";
+import SignInForm from "@/components/auth/SignInForm";
+import ButtonContainer from "@/components/global/ButtonContainer";
+import Link from "next/link";
+
+import {
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 type SignInPageProps = {};
 
 const SignInPage = ({}: SignInPageProps) => {
-    return(
-        <div>
-            Sign In Page
-        </div>
-    )
-}
+  return (
+    <>
+      <CardHeader className="text-center">
+        <CardTitle>Sign In</CardTitle>
+        <CardDescription>Sign in to continue.</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center justify-center gap-2">
+        <SignInForm />
+        <span className="text-sm">
+          Don't have an account?{" "}
+          <Link href="/auth/signup" className="text-primary underline">
+            Sign up.
+          </Link>
+        </span>
+        <ButtonContainer className="w-full">
+          <GoogleOAuthButton />
+          <GitHubOAuthButton />
+        </ButtonContainer>
+      </CardContent>
+    </>
+  );
+};
 
 export default SignInPage;
---
+++ src/components/auth/SignInForm.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { usePasswordVisibility } from "@/hooks/usePasswordVisibility";
+import { userCredentialsSchema } from "@/zod-schemas/userCredentialsSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import PasswordVisibilityToggler from "./PasswordVisibilityToggler";
+import { authenticate } from "@/actions/authenticate";
+import { toast } from "sonner";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+type SignInFormProps = {};
+
+const SignInForm = ({}: SignInFormProps) => {
+  const form = useForm<z.infer<typeof userCredentialsSchema>>({
+    resolver: zodResolver(userCredentialsSchema),
+  });
+
+  // Handle password visibility
+  const { isPasswordVisible, togglePasswordVisibility } =
+    usePasswordVisibility();
+
+  // Form submit handler
+  const onSubmit = async (data: z.infer<typeof userCredentialsSchema>) => {
+    const { email, password } = data;
+
+    // Authenticate the user
+    const authenticateResult = await authenticate({
+      provider: "credentials",
+      credentials: {
+        email: email,
+        password: password,
+      },
+      isSignUp: false,
+    });
+
+    // Set error if user does not exist
+    if (authenticateResult?.error?.name === "UserNotFoundError") {
+      form.setError("email", {
+        type: "manual",
+        message: "User with this email does not exist",
+      });
+      return;
+    }
+
+    // Set error if password is incorrect
+    if (authenticateResult?.error?.name === "IncorrectPasswordError") {
+      form.setError("password", {
+        type: "manual",
+        message: "Invalid Credentials",
+      });
+      return;
+    }
+
+    // Show toast for other errors
+    if (authenticateResult?.error) {
+      toast.error("An error occurred while signing you in. Please try again.");
+      return;
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full flex flex-col gap-2"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="john.doe@example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <div className="relative">
+                  <Input
+                    type={isPasswordVisible ? "text" : "password"}
+                    placeholder={
+                      isPasswordVisible ? "MyStrongPassword" : "********"
+                    }
+                    {...field}
+                  />
+                  <PasswordVisibilityToggler
+                    isPasswordVisible={isPasswordVisible}
+                    togglePasswordVisibility={togglePasswordVisibility}
+                  />
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full mt-2">
+          Sign In
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SignInForm;
