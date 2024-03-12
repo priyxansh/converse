28eee6cbac5986503fb5fad8cbb8959bb98ff101
Priyansh Rai <priyanshrai16@gmail.com>
2024-03-12 15:10:25 +0530

    Update SearchBar component to change query params on input change

--- src/components/app/search/SearchBar.tsx
+++ src/components/app/search/SearchBar.tsx
@@ -1,11 +1,42 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { useDebounceValue } from "usehooks-ts";
 
 type SearchBarProps = {};
 
 const SearchBar = ({}: SearchBarProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  // Get search query from query params
+  const searchQuery = searchParams.get("q") || "";
+
+  // Debounce search query
+  const [debouncedSearchQuery, setValue] = useDebounceValue(searchQuery, 300);
+
+  // Update query params when debounced search query changes
+  useEffect(() => {
+    if (!debouncedSearchQuery) {
+      router.push("/search");
+      return;
+    }
+
+    router.push(`/search?q=${debouncedSearchQuery}`);
+  }, [debouncedSearchQuery]);
+
   return (
     <div>
-      <Input placeholder="Find people" className="w-full max-w-lg mx-auto" />
+      <Input
+        placeholder="Find people"
+        className="w-full max-w-lg mx-auto"
+        defaultValue={searchQuery}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
     </div>
   );
 };
