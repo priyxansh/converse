18264803556f10d8f40cb01dbe2eb07560db6f3f
Priyansh Rai <priyanshrai16@gmail.com>
2024-03-25 13:19:18 +0530

    Add suspense and spinner to FriendsPage component

--- src/app/(app)/friends/page.tsx
+++ src/app/(app)/friends/page.tsx
@@ -1,11 +1,21 @@
 import FriendsPageTabs from "@/components/app/friends/FriendsPageTabs";
+import Spinner from "@/components/global/Spinner";
+import { Suspense } from "react";
 
 type FriendsPageProps = {};
 
 const FriendsPage = ({}: FriendsPageProps) => {
   return (
     <main className="flex-grow py-4 px-2 sm:px-4 flex flex-col">
-      <FriendsPageTabs />
+      <Suspense
+        fallback={
+          <div className="w-full flex-grow grid place-items-center">
+            <Spinner />
+          </div>
+        }
+      >
+        <FriendsPageTabs />
+      </Suspense>
     </main>
   );
 };
