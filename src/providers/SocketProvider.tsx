fe214148a3cfc1f4f829c9870110e57734e2928b
Priyansh Rai <priyanshrai16@gmail.com>
2024-04-05 12:17:23 +0530

    Refactor SocketProvider to handle missing socket URL

--- src/providers/SocketProvider.tsx
+++ src/providers/SocketProvider.tsx
@@ -4,15 +4,34 @@ import { createContext, useEffect } from "react";
 import { io, Socket } from "socket.io-client";
 
 // Create a context for the socket
-const SocketContext = createContext<Socket | null>(null);
+const SocketContext = createContext<{
+  socket: Socket;
+  isConnected: boolean;
+} | null>(null);
 
 type SocketProviderProps = {
   children: React.ReactNode;
 };
 
 const SocketProvider = ({ children }: SocketProviderProps) => {
+  // Get the socket URL from the environment variables
+  const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL;
+
+  // If the socket URL is not found, show an error message
+  if (!socketUrl) {
+    return (
+      <div className="flex-grow flex flex-col items-center justify-center p-4 text-center gap-2">
+        <h1 className="font-semibold sm:text-lg">Socket URL not found :(</h1>
+        <p className="text-sm sm:text-base">
+          Please add the socket URL to the environment variables in the .env
+          file.
+        </p>
+      </div>
+    );
+  }
+
   // Create a socket connection
-  const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL as string);
+  const socket = io(socketUrl);
 
   // Disconnect the socket when the component unmounts
   useEffect(() => {
@@ -22,7 +41,14 @@ const SocketProvider = ({ children }: SocketProviderProps) => {
   }, []);
 
   return (
-    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+    <SocketContext.Provider
+      value={{
+        socket,
+        isConnected: socket.connected,
+      }}
+    >
+      {children}
+    </SocketContext.Provider>
   );
 };
 
